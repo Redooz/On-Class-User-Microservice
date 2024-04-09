07527cd72ef0b436d7a158ebc9b30bb21079672e
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-09 17:55:00 -0500

    project structure

---
+++ src/main/java/com/redoz/onclassuser/OnclassUserApplication.java
@@ -0,0 +1,13 @@
+package com.redoz.onclassuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnclassUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnclassUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redoz/onclassuser/OnclassUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redoz.onclassuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnclassUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
