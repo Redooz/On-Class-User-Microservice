6ee19913675707288073fcb3e27c82f598d552fc
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-11 23:16:31 -0500

    feat(user): implement user details interface in user entity

--- src/main/java/com/redoz/onclassuser/infrastructure/driven/jpa/mysql/entity/UserEntity.java
+++ src/main/java/com/redoz/onclassuser/infrastructure/driven/jpa/mysql/entity/UserEntity.java
@@ -5,8 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -14,7 +19,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     private String documentNumber;
 
@@ -32,4 +37,38 @@ public class UserEntity {
 
     private String password;
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
