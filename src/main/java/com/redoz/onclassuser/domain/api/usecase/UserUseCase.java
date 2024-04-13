9fe1d126992bc4f759977f9d02bbd21951d8a517
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-13 00:15:24 -0500

    feat(user): add service adapter and port for user, also a method for saving a user

---
+++ src/main/java/com/redoz/onclassuser/domain/api/usecase/UserUseCase.java
@@ -0,0 +1,18 @@
+package com.redoz.onclassuser.domain.api.usecase;
+
+import com.redoz.onclassuser.domain.api.IUserServicePort;
+import com.redoz.onclassuser.domain.model.User;
+import com.redoz.onclassuser.domain.spi.IUserPersistencePort;
+
+public class UserUseCase implements IUserServicePort {
+    private final IUserPersistencePort userPersistencePort;
+
+    public UserUseCase(IUserPersistencePort userPersistencePort) {
+        this.userPersistencePort = userPersistencePort;
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userPersistencePort.saveUser(user);
+    }
+}
