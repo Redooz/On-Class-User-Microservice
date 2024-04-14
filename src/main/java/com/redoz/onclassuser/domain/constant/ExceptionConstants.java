f7cc3cfaa80ebc76aa37740c4a85fd489e67dc04
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-14 13:40:21 -0500

    feat(user): add method to get user from email in user service port and adapter (usecase)

---
+++ src/main/java/com/redoz/onclassuser/domain/constant/ExceptionConstants.java
@@ -0,0 +1,9 @@
+package com.redoz.onclassuser.domain.constant;
+
+public class ExceptionConstants {
+    private ExceptionConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String NO_DATA_FOUND_USER_EXCEPTION_MESSAGE = "'user'";
+}
---
+++ src/main/java/com/redoz/onclassuser/domain/exception/NoDataFoundException.java
@@ -0,0 +1,7 @@
+package com.redoz.onclassuser.domain.exception;
+
+public class NoDataFoundException extends RuntimeException {
+    public NoDataFoundException(String message) {
+        super(message);
+    }
+}
