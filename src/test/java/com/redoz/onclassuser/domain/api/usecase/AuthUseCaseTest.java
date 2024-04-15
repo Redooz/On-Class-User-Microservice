19a7009bf766b06724c6d0d11800e7befc195e9e
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-14 19:17:12 -0500

    feat(auth): add new tests for login in controller and in use case of user and auth

--- src/test/java/com/redoz/onclassuser/domain/api/usecase/AuthUseCaseTest.java
+++ src/test/java/com/redoz/onclassuser/domain/api/usecase/AuthUseCaseTest.java
@@ -3,16 +3,19 @@ package com.redoz.onclassuser.domain.api.usecase;
 
 import com.redoz.onclassuser.configuration.security.service.JwtService;
 import com.redoz.onclassuser.domain.api.IUserServicePort;
+import com.redoz.onclassuser.domain.exception.InvalidPasswordException;
 import com.redoz.onclassuser.domain.model.Role;
 import com.redoz.onclassuser.domain.model.User;
 import com.redoz.onclassuser.infrastructure.driven.jpa.mysql.mapper.IUserEntityMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class AuthUseCaseTest {
@@ -69,6 +72,36 @@ class AuthUseCaseTest {
         verify(userServicePort, times(1)).saveUser(user);
         verify(userEntityMapper, times(1)).toEntity(user);
         assertEquals(encodedPassword, user.getPassword());
+    }
+
+    @Test
+    void shouldReturnTokenWhenLoginIsSuccessful() {
+        String email = "john@doe.com";
+        String password = "password";
+        String token = "token";
+        User user = getUser();
+        user.setPassword(password);
+
+        when(userServicePort.findUserByEmail(email)).thenReturn(user);
+        when(passwordEncoder.matches(password, user.getPassword())).thenReturn(true);
+        when(jwtService.generateToken(userEntityMapper.toEntity(user))).thenReturn(token);
+
+        String result = authUseCase.login(email, password);
+
+        assertEquals(token, result);
+    }
+
+    @Test
+    void shouldThrowInvalidPasswordExceptionWhenPasswordIsIncorrect() {
+        String email = "john@doe.com";
+        String password = "password";
+        User user = getUser();
+        user.setPassword("wrongpassword");
+
+        when(userServicePort.findUserByEmail(email)).thenReturn(user);
+        when(passwordEncoder.matches(password, user.getPassword())).thenReturn(false);
+
+        assertThrows(InvalidPasswordException.class, () -> authUseCase.login(email, password));
     }
 
     User getUser() {
--- src/test/java/com/redoz/onclassuser/infrastructure/driving/http/controller/AuthRestControllerAdapterTest.java
+++ src/test/java/com/redoz/onclassuser/infrastructure/driving/http/controller/AuthRestControllerAdapterTest.java
@@ -1,5 +1,6 @@
 package com.redoz.onclassuser.infrastructure.driving.http.controller;
 
+import com.redoz.onclassuser.application.dto.request.LoginRequest;
 import com.redoz.onclassuser.application.dto.request.RegisterUserRequest;
 import com.redoz.onclassuser.application.dto.response.AuthResponse;
 import com.redoz.onclassuser.application.handler.AuthHandler;
@@ -36,6 +37,19 @@ class AuthRestControllerAdapterTest {
         ResponseEntity<AuthResponse> result = authRestControllerAdapter.registerAdmin(registerUserRequest);
 
         assertEquals(HttpStatus.CREATED, result.getStatusCode());
+        assertEquals(authResponse, result.getBody());
+    }
+
+    @Test
+    void shouldReturnOkStatusAndAuthResponseWhenUserIsLoggedIn() {
+        LoginRequest loginRequest = new LoginRequest("john@doe.com", "password");
+        AuthResponse authResponse = new AuthResponse("token");
+
+        when(authHandler.login(loginRequest)).thenReturn(authResponse);
+
+        ResponseEntity<AuthResponse> result = authRestControllerAdapter.login(loginRequest);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
         assertEquals(authResponse, result.getBody());
     }
 }
